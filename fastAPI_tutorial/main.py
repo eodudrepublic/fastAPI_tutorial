cb1277d27fca10c09d7a0603f9e59208e7827077
Daeyoung Kim <eodudrepublic@gmail.com>
2023-12-28 03:15:54 +0900

    AWS 서버에 배포
    AWS에서 생짜 CPU를 발급받아서
    리눅스를 깔고
    거기에 fastAPI 사용할 수 있도록 환경설정하고
    git에 올려놓은 내 프로젝트를 clone받아서
    AWS cpu에서 내 홈페이지를 돌려봄

--- fastAPI_tutorial/main.py
+++ fastAPI_tutorial/main.py
@@ -1,14 +1,19 @@
 from fastapi import FastAPI
+
 app = FastAPI()
 
 from fastapi.responses import FileResponse
+
+
 # DB 입출력 기능 : DB 접속해주세요~
 
-@app.get("/")       # main page에 접속 했을때
+@app.get("/")  # main page에 접속 했을때
 def test():
     # DB 입출력 기능 : DB에서 데이터 꺼내주세요~
     # return "hello"  # return할 데이터
-    return FileResponse('index.html')   # main page에 접속시 index.html 파일 전송
+    return FileResponse('index.html')  # main page에 접속시 index.html 파일 전송
+
+
 # async def -> await
 # await : 기다려라 -> 비동기 처리 가능
 
@@ -16,21 +21,26 @@ def test():
 def test1():
     return FileResponse('test1.html')
 
+
 @app.get("/test2")
 def test1():
     return FileResponse('test2.html')
+
 
 @app.get("/test3")
 def test1():
     return FileResponse('test3.html')
 
+
 @app.get("/test4")
 def test1():
     return FileResponse('test4.html')
 
-@app.get("/data")       # /data에 접속 했을때
+
+@app.get("/data")  # /data에 접속 했을때
 def test():
-    return {"hello" : 1234}  # return할 데이터
+    return {"hello": 1234}  # return할 데이터
+
 
 # API 문서 확인 : /docs, /redoc
 
@@ -38,11 +48,15 @@ def test():
 
 # 유저에게 데이터를 받고 싶으면 모델부터 생성해야 함
 from pydantic import BaseModel
+
+
 # Model 클래스 안에 유저들이 어떤 데이터를 보낼 수 있는지 명시해줘야 함
 class Model(BaseModel):
-    name : str
-    phone : int
-@app.post("/send")      # /send에 접속 했을때 : post -> 유저가 데이터를 보낼 수 있는 기능
-def test(data : Model): # data : 보낸 데이터 담김, 타입 기능 기본 제공
+    name: str
+    phone: int
+
+
+@app.post("/send")  # /send에 접속 했을때 : post -> 유저가 데이터를 보낼 수 있는 기능
+def test(data: Model):  # data : 보낸 데이터 담김, 타입 기능 기본 제공
     print(data)
     return 'Send Successfully'  # return할 데이터
